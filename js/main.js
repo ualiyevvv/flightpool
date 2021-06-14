3c1680287c5e03d29753a4697cf17274ac41adfe
fujiwara <62168382+ualiyevvv@users.noreply.github.com>
2021-06-14 18:18:21 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-// Sticky
-const stickyElement = document.querySelector(".header");
-const checkSticky = e => {
-    let scrollTop = document.querySelector("html").scrollTop;
 
-    console.log(scrollTop);
-    if(scrollTop > 100) {
-        stickyElement.classList.add("sticky");
-    } else {
-        stickyElement.classList.remove("sticky");
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu__list'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+
+
+        
+    // Sticky
+    const stickyElement = document.querySelector(".header");
+    const checkSticky = e => {
+        let scrollTop = document.querySelector("html").scrollTop;
+
+        console.log(scrollTop);
+        if(scrollTop > 100) {
+            stickyElement.classList.add("sticky");
+        } else {
+            stickyElement.classList.remove("sticky");
+        }
     }
-}
-window.addEventListener("scroll", checkSticky);
-checkSticky();
+    window.addEventListener("scroll", checkSticky);
+    checkSticky();
+})
